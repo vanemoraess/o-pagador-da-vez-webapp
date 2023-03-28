1f7d61e01c31b202836c154812c13cc88a88dda9
Vanessa Moraes <80935316+vanemoraess@users.noreply.github.com>
2023-03-28 14:40:42 -0300

    Atualiza o componente de entrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
-import { useState } from "react";
-import { Delete } from "./components/Delete";
+import { useReducer, useState } from "react";
 import { Input } from "./components/Input";
 
 function OPagadorDaVez() {
-  const [names, setNames] = useState([]);
-  const [currentName, setCurrentName] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [chosenPerson, setChosenPerson] = useState();
+
+  const removePerson = (name) => {
+    setPersons(persons.filter((value) => value != name));
+  };
+
+  const addPerson = (name) => {
+    if (!persons.includes(name)) setPersons([...persons, name]);
+  };
+
+  const choosePerson = () => {
+    do {
+      const value = Number(Math.random() * 100).toFixed(0);
+      if (persons[value]) {
+        setChosenPerson(persons[value]);
+        break;
+      }
+    } while (true);
+  };
 
   return (
     <div style={mainStyles}>
       <h1>O Pagador da vez</h1>
       <div style={{ ...inputBlockStyles, marginTop: "30px" }}>
         <Input
-          value={currentName}
-          onChange={(value) => {
-            setCurrentName(value);
+          onValue={(value) => {
+            addPerson(value);
           }}
         />
-        <span
-          tabIndex={0}
-          onClick={() => {
-            const nameToAdd = currentName;
-            setNames([...names, nameToAdd]);
-            setCurrentName("");
-          }}
-          style={{
-            fontWeight: "800",
-            fontSize: "30px",
-            paddingLeft: "10px",
-            cursor: "pointer",
-          }}
-        >
-          +
-        </span>
       </div>
-      {names.map((name) => (
+      {persons.map((person) => (
         <>
-          <div styles={inputBlockStyles}>
-            <Input value={name} disabled={false} />
-            <Delete />
-          </div>
+          <span key={person}>{person}</span>
+          <span onClick={() => removePerson(person)}>x</span>
         </>
       ))}
+
+      <span
+        onClick={() => {
+          console.log(persons[choosePerson()]);
+        }}
+      >
+        SORTEAR
+      </span>
+      <span>{chosenPerson ?? ""}</span>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,30 @@
-export function Input({ value, onChange, disabled = false }) {
+export function Input({ onValue }) {
+  let value = "";
   return (
-    <input
-      disabled={disabled}
-      style={styles}
-      onChange={(event) => {
-        onChange(event.target.value);
-      }}
-    ></input>
+    <>
+      <input
+        style={styles}
+        onChange={(event) => {
+          event.preventDefault();
+          value = event.target.value;
+        }}
+      ></input>
+      <span
+        tabIndex={0}
+        onClick={() => {
+          onValue(value);
+          value = "";
+        }}
+        style={{
+          fontWeight: "800",
+          fontSize: "30px",
+          paddingLeft: "10px",
+          cursor: "pointer",
+        }}
+      >
+        +
+      </span>
+    </>
   );
 }
 
